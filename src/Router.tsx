a50249b00d400eee9f1c71dcb9f0e140c93e9a25
Icaro Sperandio <icaro.sperandio@gmail.com>
2022-08-09 18:47:17 -0300

    ajustes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import { Contato } from "./pages/contato";
 import { Home } from "./pages/home";
 import { Projetos } from "./pages/projetos";
-import { Resumo } from "./pages/resumo";
 import { Skills } from "./pages/skills";
 import { Sobre } from "./pages/sobre";
 
@@ -10,9 +8,7 @@ export function Router() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/contato" element={<Contato />} />
             <Route path="/projetos" element={<Projetos />} />
-            <Route path="/resumo" element={<Resumo />} />
             <Route path="/skills" element={<Skills />} />
             <Route path="/sobre" element={<Sobre />} />
         </Routes>
