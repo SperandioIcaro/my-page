37dad8411b0689a6a5eb30bfdf3d6b717efc341b
SperandioIcaro <icaro.sperandio@gmail.com>
2022-10-21 10:46:31 -0300

    ajuste na pagina sobre

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -10,12 +10,16 @@ export function About() {
                 flex flex-col lg:m-2
                 m-1
                 ">
-                <h1 className="text-center font-sans text-cyan-800 text-4xl">
+                <h1 className="
+                        text-center font-sans text-cyan-800 text-4xl
+                        xl:text-5xl
+                        ">
                     Um pouco sobre mim!
                 </h1>
                 <span className="
-                    flex text-center lg:ml-96 font-Press text-cyan-900 lg:-mt-2 lg:mb-4 text-lg underline
-                    ml-24
+                    flex text-center font-Press text-cyan-900 text-lg underline ml-24
+                    md:ml-96 md:-mt-2 md:mb-4 
+                    xl:self-center xl:text-xl
                     ">
                     pode ser que seja interessante.
                 </span>
@@ -25,8 +29,11 @@ export function About() {
                 flex lg:mt-8 lg:p-1
                 mt-2
                 ">
-                <p className="flex text-justify font-inco text-base text-black">
-                    Olá, meu nome é Ícaro Sperandio, tenho 25 anos e sou estudante de analise e desenvolvimento de sistemas e design de games. Dito isso darei um breve resumo sobre mim!
+                <p className="
+                        flex text-justify font-inco text-base text-black
+                        xl:text-lg
+                        ">
+                    Olá, meu nome é Ícaro Sperandio, tenho 26 anos e sou estudante de analise e desenvolvimento de sistemas e design de games. Dito isso darei um breve resumo sobre mim!
                     <br /><br />
                     Tudo começou pela arte, em minha família por parte de mãe todos possuem um talento especifico para a arte, alguns pintam quadros, outros são musicistas e alguns só desenham, eu peguei um pouco de cada um, mas o que eu mais gostava de fazer era desenhar, sempre desenhava um personagem de algum filme o qual eu gostava, de animações, e também desde sempre eu amei animações desde os desenhos animados americanos até os animes japoneses.<br /><br />   Eu sempre senti isso estimular minha imaginação a ponto dela pular para fora da minha mente, seja em forma de desenhos ou ganhando vida em minha mente, isso sempre me ajudou nas aulas de artes na escola, e com o tempo eu fiz um curso de design gráfico e eu percebi que essa mesma facilidade que eu tinha nas aulas também me ajudou na criação de panfletos, cardápios e etc.<br /><br />    Mas, vi que focar em apenas isso não era para mim, o tempo passou eu acabei perdendo o gosto por desenhar, fiquei confuso em que profissão seguir, ate que eu conheci algumas pessoas da área de programação que me apresentou a profissão e peguei um certo interesse, comecei a estudar o back-end, depois de alguns dias estudando recebi um anuncio referente a um curso de programação de jogos, fiquei extremamente animado, já que juntava a profissão que estava me conquistando e jogos o qual é algo que eu sempre gostei, iniciei o curso o mais rápido possível.<br /><br />     Durante o curso conheci algumas pessoas extremamente talentosas para a arte digital a qual inclui a própria professora a qual graças a ela, dando dicas, estimulando a prática e até mesmo alguns exercícios práticos, o meu gosto por desenhar voltou com tudo e evoluiu para o desenho digital, e nesse momento de felicidade que me veio a luz, que eu poderia unir a minha arte e a programação com uma maior facilidade no front-end.<br /><br />    A partir dai mudei meu cronograma de estudos, participei de um projeto voluntario, e graças a essas experiências hoje eu me sinto pronto para ingressar profissionalmente na área de desenvolvimento front-end. Sigo estudando diariamente para me manter atualizado e ganhar ainda mais experiência!
                 </p>
